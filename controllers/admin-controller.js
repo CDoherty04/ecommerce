9d5d6d7c5acb43ba314583897e5e5b7cccef1cde
Charlie <charlieedoherty@gmail.com>
2024-08-11 22:06:30 -0500

    Implement product updating

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -3,7 +3,7 @@ const Product = require("../models/product-model")
 async function getProducts(req, res, next) {
     try {
         const products = await Product.findAll()
-        res.render("admin/products/all-products", {products: products})
+        res.render("admin/products/all-products", { products: products })
     } catch (error) {
         next(error)
         return
@@ -29,8 +29,39 @@ async function createNewProduct(req, res, next) {
     res.redirect("/admin/products")
 }
 
+async function getUpdateProduct(req, res, next) {
+    try {
+        const product = await Product.findByID(req.params.id)
+        res.render("admin/products/update-product", { product: product })
+    } catch (error) {
+        next(error)
+    }
+}
+
+async function updateProduct(req, res, next) {
+    const product = new Product({
+        ...req.body,
+        _id: req.params.id,
+    })
+
+    if (req.file) {
+        product.replaceImage(req.file.filename)
+    }
+
+    try {
+        await product.save()
+    } catch (error) {
+        next(error)
+        return
+    }
+
+    res.redirect("/admin/products")
+}
+
 module.exports = {
     getProducts: getProducts,
     getNewProduct: getNewProduct,
-    createNewProduct: createNewProduct
+    createNewProduct: createNewProduct,
+    getUpdateProduct: getUpdateProduct,
+    updateProduct: updateProduct
 }
--- models/product-model.js
+++ models/product-model.js
@@ -1,3 +1,5 @@
+const mongodb = require("mongodb")
+
 const db = require("../data/database")
 
 class Product {
@@ -7,8 +9,7 @@ class Product {
         this.summary = productData.summary
         this.price = +productData.price
         this.description = productData.description
-        this.imagePath = `product-data/images/${productData.image}`
-        this.imageURL = `/products/assets/images/${productData.image}`
+        this.updateImageData()
         if (productData._id) {
             this.id = productData._id.toString()
         }
@@ -22,16 +23,59 @@ class Product {
         })
     }
 
+    static async findByID(productID) {
+        let prodId
+        try {
+            prodId = mongodb.ObjectId.createFromHexString(productID)
+        } catch (error) {
+            error.code = 404
+            throw error
+        }
+        const product = await db.getDb().collection("products").findOne({ _id: prodId })
+
+        if (!product) {
+            const error = new Error("Could not find product with given ID")
+            error.code = 404
+            throw error
+        }
+        return new Product(product)
+    }
+
+    updateImageData() {
+        this.imagePath = `product-data/images/${this.image}`
+        this.imageURL = `/products/assets/images/${this.image}`
+    }
+
     async save() {
         const productData = {
             title: this.title,
             summary: this.summary,
             price: this.price,
             description: this.description,
-            image: this.image,
+            image: this.image
         }
 
-        await db.getDb().collection("products").insertOne(productData)
+        if (this.id) {
+            const productId = mongodb.ObjectId.createFromHexString(this.id)
+
+            if (!this.image) {
+                delete productData.image
+            }
+            
+            await db.getDb().collection("products").updateOne(
+                { _id: productId },
+                {
+                    $set: productData
+                }
+            )
+        } else {
+            await db.getDb().collection("products").insertOne(productData)
+        }
+    }
+
+    async replaceImage(newImage) {
+        this.image = newImage
+        this.updateImageData()
     }
 }
 
