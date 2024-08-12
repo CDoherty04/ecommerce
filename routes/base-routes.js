946c75c18fddda437fe162a6d50729c8383e2d64
Charlie <charlieedoherty@gmail.com>
2024-08-11 23:29:42 -0500

    Add 404 handling

--- routes/base-routes.js
+++ routes/base-routes.js
@@ -2,8 +2,12 @@ const express = require("express")
 
 const router = express.Router()
 
-router.get("/", function(req, res) {
+router.get("/", function (req, res) {
     res.redirect("/products")
+})
+
+router.get("/500", function (req, res) {
+    res.render("/shared/500")
 })
 
 module.exports = router
